1ffcbc76f0551642ab8ba6395a5d1b0c6eae4ac9
sukeun1997 <busan-sy@naver.com>
2021-09-30 17:39:58 +0900

    feat(레이싱 게임) : 자동차그룹 최대 거리 , 승자 이름 확인

--- src/main/java/racing/Cars.java
+++ src/main/java/racing/Cars.java
@@ -2,6 +2,7 @@ package racing;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
     private final List<Car> carList;
@@ -16,5 +17,17 @@ public class Cars {
 
     public void move(List<Car> carList) {
         carList.stream().forEach(car -> car.move((int) Math.round(Math.random())));
+    }
+
+    public int Maxdistance() {
+        return carList.stream().mapToInt(car -> car.getLocation()).max().getAsInt();
+    }
+
+
+    public List<String> WinName(int distance) {
+        return carList.stream().
+                filter(car -> car.getLocation() == distance).
+                map(name -> name.getName()).
+                collect(Collectors.toList());
     }
 }
--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -14,16 +14,6 @@ public class CarTest {
     }
 
     @Test
-    void 문자열_자동차_객체_생성() {
-
-        String carsName = "a,b,c";
-        String[] car = carsName.split(",");
-        List<Car> carList = Create.NewCar(car);
-        Cars cars = new Cars(carList);
-        assertThat(cars.getCar().size()).isEqualTo(3);
-    }
-
-    @Test
     void 자동차_움직임_구현() {
         Car car = new Car("test1");
         car.move((int) Math.round(Math.random()));
@@ -41,25 +31,4 @@ public class CarTest {
         assertThat(car.getLocation()).isLessThanOrEqualTo(5);
     }
 
-    @Test
-    void 자동차_그룹_움직임_구현() {
-        String carsName = "a,b,c";
-        String[] car = carsName.split(",");
-        List<Car> carList = Create.NewCar(car);
-        Cars cars = new Cars(carList);
-        cars.move(carList);
-        cars.getCar().stream().forEach(a -> System.out.println(a.toString()));
-    }
-
-    @Test
-    void 자동차_그룹_5번_움직임_구현() {
-        String carsName = "a,b,c";
-        String[] car = carsName.split(",");
-        List<Car> carList = Create.NewCar(car);
-        Cars cars = new Cars(carList);
-        for (int i = 0; i <= 5; i++) {
-            cars.move(carList);
-        }
-        cars.getCar().stream().forEach(a -> System.out.println(a.toString()));
-    }
 }
