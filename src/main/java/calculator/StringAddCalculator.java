75b4f9bcbd68125b53414c1d70f06dbd3a4909dd
Jihun Park <jihunpark.tech@gmail.com>
2022-04-25 23:19:57 +0900

    refactor: extract method

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -3,18 +3,26 @@ package calculator;
 public class StringAddCalculator {
     public static int splitAndSum(String text) {
 
-        if (text == null) {
+        if (isNullOrEmpty(text)) {
             return 0;
         }
 
-        if (text.isEmpty()) {
-            return 0;
-        }
-
-        if (text.length() == 1) {
+        if (isSingleNumber(text)) {
             return Integer.parseInt(text);
         }
 
         return 0;
+    }
+
+    private static boolean isSingleNumber(String text) {
+        return text.length() == 1;
+    }
+
+    private static boolean isNullOrEmpty(String text) {
+        if (text == null || text.isEmpty()) {
+            return true;
+        }
+
+        return false;
     }
 }
