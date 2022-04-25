ae5b92b473801b0625a2933902cb7f0157be4ad3
Jihun Park <jihunpark.tech@gmail.com>
2022-04-25 23:15:37 +0900

    add: 빈 문자열 또는 null 값을 입력할 경우 0을 반환

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String text) {
+
+        if (text == null) {
+            return 0;
+        }
+
+        if (text.isEmpty()) {
+            return 0;
+        }
+
+        return 0;
+    }
+}
