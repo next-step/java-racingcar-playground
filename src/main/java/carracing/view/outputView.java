d407db251f5373865c905be3fb5cfa9cbe02e910
980327 <alap_u@naver.com>
2022-07-21 15:26:43 +0900

    feat - 자동차 이름 입력 요청 메시지

---
+++ src/main/java/carracing/view/outputView.java
@@ -0,0 +1,12 @@
+package carracing.view;
+
+public class outputView {
+
+    public static final String REQEUST_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+
+    public static void requestCarNameMSG(){
+        System.out.println(REQEUST_CAR_NAME);
+    }
+
+
+}
