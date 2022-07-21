78bf9ae77092c8ac3b2148e97de06680405d254e
980327 <alap_u@naver.com>
2022-07-21 15:32:52 +0900

    STYLE - 메서드 이름 수정

--- src/main/java/carracing/view/outputView.java
+++ src/main/java/carracing/view/outputView.java
@@ -2,11 +2,11 @@ package carracing.view;
 
 public class outputView {
 
-    public static final String REQEUST_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    public static final String REQUEST_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     public static final String REQUEST_GAME_COUNT = "시도할 회수는 몇회인가요?";
 
     public static void requestCarNameMSG(){
-        System.out.println(REQEUST_CAR_NAME);
+        System.out.println(REQUEST_CAR_NAME);
     }
 
     public static void requestGameCount(){
