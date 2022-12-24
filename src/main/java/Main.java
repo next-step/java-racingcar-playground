24e28a74b831d2fd9938aa1ba893ba65bceae0e2
seonghJoo <etkl8888@naver.com>
2022-12-24 22:32:29 +0900

    [UPDATE] Scanner 도입

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,9 @@ public class Main {
         Racing racing = new Racing();
         Scanner scanner = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String input = "pobi,crong,honux";
-                //scanner.nextLine();
+        String input = scanner.nextLine();
         System.out.println("시도할 회수는 몇회인가요?");
-        int game = 5;
-                //scanner.nextInt();
+        int game = scanner.nextInt();
         List<Car> cars = racing.start(input,game);
 
         boolean play = true;
