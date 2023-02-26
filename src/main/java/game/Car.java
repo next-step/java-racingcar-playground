5fb5b651c6b280dde762545ac44df16dd88e5c9e
남의영 <ny2485@naver.com>
2023-02-26 17:35:12 +0900

    feat(young2) 자동차 이름5자 이하 검증

---
+++ src/main/java/game/Car.java
@@ -0,0 +1,10 @@
+package game;
+
+public class Car {
+    //    private String carName;
+    public CarName carName;
+
+    public Car(String carName) {
+        this.carName = new CarName(carName);
+    }
+}
