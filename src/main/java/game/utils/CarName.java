d9304c7d7cf6b2ddd04a6a5399e69638dfbf391b
남의영 <ny2485@naver.com>
2023-03-02 02:03:50 +0900

    feat(young2) : utils 검증 파일 추가

--- src/main/java/game/utils/CarName.java
+++ src/main/java/game/utils/CarName.java
@@ -4,10 +4,15 @@ public class CarName {
 
     private final int MAX_LEN = 5;
     private String carName;
-    protected CarName(String carName) {
+
+
+    public CarName(String carName) {
         if (carName.length() > MAX_LEN) {
             throw new IllegalArgumentException("자동차 이름의 길이는 5보다 작아야 합니다.");
         }
         this.carName = carName;
+    }
+    public String getCarName() {
+        return carName;
     }
 }
--- src/main/java/game/utils/RandomNum.java
+++ src/main/java/game/utils/RandomNum.java
@@ -1,2 +1,8 @@
-package game.utils;public class RandomPosition {
+package game.utils;
+
+public class RandomNum {
+
+    public static int RandomMove() {
+        return  (int)(Math.random() * 10000 % 9);
+    }
 }
