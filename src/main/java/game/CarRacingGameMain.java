f5c02a7f1677b34988e0784291fa1ea4ad4c2600
남의영 <ny2485@naver.com>
2023-03-02 02:05:20 +0900

    feat(young2) : main클래스, 실행클래스 추가

--- src/main/java/game/CarRacingGameMain.java
+++ src/main/java/game/CarRacingGameMain.java
@@ -2,6 +2,7 @@ package game;
 
 import game.domain.Car;
 import game.domain.CarRacingGame;
+import game.domain.Cars;
 import game.views.InputView;
 import game.views.OutputView;
 import org.apache.commons.lang3.StringUtils;
@@ -9,22 +10,12 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.List;
 import java.util.Scanner;
 
-public class RacingMain {
+public class CarRacingGameMain {
+
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
-
-        OutputView.printInitCars();
-        String carNames = scanner.nextLine();
-        if (StringUtils.isBlank(carNames)) { //TODO valid로 넣기
-            throw new IllegalArgumentException("자동차 이름을 입력해 주세요.");
-        }
-        List<Car> carList = InputView.inputCars(carNames);
-        OutputView.printInitRepete();
-        InputView.inputRepete();
-
-
-        OutputView.printRacingCars(carList);
-
+        CarRacingGame carRacingGame = new CarRacingGame();
+        carRacingGame.inputData();
+        carRacingGame.gameResult();
+        carRacingGame.findWinners();
     }
 }
