7b661ad610bd57794c80dd14c13cc7773e4afff3
be <jacobhboy66@gmail.com>
2023-03-28 23:01:42 +0900

    feat(view): 매 턴마다 결과를 출력

---
+++ src/main/java/view/output/ResultOutput.java
@@ -0,0 +1,7 @@
+package view.output;
+
+public class ResultOutput {
+    public static void OutputResult(String name, int advance){
+        System.out.println(name + ":" + ("-".repeat(advance)));
+    }
+}
