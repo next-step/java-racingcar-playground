9914ad420547a45fc00a2560830f06e9a0b6a64c
ekore <yonghwa95@naver.com>
2023-07-29 16:54:37 +0900

    feat (feature) application 클래스 추가

--- src/main/java/StringCalculatorTest/StringAddCalApplication.java
+++ src/main/java/StringCalculatorTest/StringAddCalApplication.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class StringAddCalApplication {
     public static void main(String [] args) {
         System.out.println("숫자를 입력해주세요");
-            try {
-                String input = new Scanner(System.in).nextLine();
-                int answer = StringAddCalculator.splitAndSum(input);
-                System.out.println(answer);
-            } catch (RuntimeException e) {
-                System.out.println("값을 잘못 입력하셨습니다.");
-                e.printStackTrace();
-            }
-
+        try {
+            String input = new Scanner(System.in).nextLine();
+            int answer = StringAddCalculator.splitAndSum(input);
+            System.out.println(answer);
+        } catch (RuntimeException e) {
+            System.out.println("값을 잘못 입력하셨습니다.");
+            e.printStackTrace();
+        }
     }
 }
