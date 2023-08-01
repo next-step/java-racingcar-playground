beb805630d2e6db9a48007821705238d9a831cd4
ekore <yonghwa95@naver.com>
2023-08-01 20:12:16 +0900

    test NameTest 작성완료

---
+++ src/test/java/RacingGameTest/NameTest.java
@@ -0,0 +1,23 @@
+package RacingGameTest;
+
+import RacingGame.Name;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NameTest {
+    Name nameOne;
+    Name nameTwo;
+    @BeforeEach
+    void generateInstance() {
+        nameOne = new Name("truck");
+        nameTwo = new Name("sportsCar");
+    }
+
+    @Test
+    void isCarNameOverFiveTest() {
+        assertThat(nameOne.isCarNameLengthOverFive("truck")).isFalse();
+        assertThat(nameTwo.isCarNameLengthOverFive("superCar")).isTrue();
+    }
+}
