1ea99a9e1d02fce3c4229a274529fa77678a012c
ekore <yonghwa95@naver.com>
2023-08-05 15:20:08 +0900

    feat GameService playRound 메서드 구현

--- src/main/java/RacingGame/GameService.java
+++ src/main/java/RacingGame/GameService.java
@@ -1,6 +1,10 @@
 package RacingGame;
 
+import java.util.Random;
+
 public class GameService {
+
+    private int rounds;
 
     public Cars joinCar (String [] splitedCarNames) {
         Cars cars = Cars.getInstance();
@@ -9,5 +13,17 @@ public class GameService {
             cars.setJoinedCar(car);
         }
         return cars;
+    }
+
+    public void playRound(Cars cars) {
+        Random random = new Random();
+        ResultView view = new ResultView();
+        for (int round=1; round <= rounds; round++) {
+            for (int index=0; index < cars.getJoinedCar().size(); index++) {
+                cars.getJoinedCar().get(index).go(random.nextInt());
+            }
+            view.printRoundResult(cars);
+        }
+        view.printWinner(cars);
     }
 }
