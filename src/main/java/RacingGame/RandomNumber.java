8c12ee6215b12267522c451e08f78417775dbd11
ekore <yonghwa95@naver.com>
2023-08-05 13:34:34 +0900

    feat RandomNumber Test code, generateRandomNumber 작성

---
+++ src/main/java/RacingGame/RandomNumber.java
@@ -0,0 +1,10 @@
+package RacingGame;
+
+import java.util.Random;
+
+public class RandomNumber {
+    public int genateRandomNumber() {
+        Random generator = new Random();
+        return generator.nextInt(9)+1;
+    }
+}
