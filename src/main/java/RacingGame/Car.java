61c87a7484e006e81e110ad87b3472bc4306294d
ekore <yonghwa95@naver.com>
2023-08-05 13:52:56 +0900

    feat Car go( )메서드 만들기

--- src/main/java/RacingGame/Car.java
+++ src/main/java/RacingGame/Car.java
@@ -4,10 +4,17 @@ public class Car {
     private Name carName;
     private Position carPosition;
 
-    public Car (String splitedName ) {
+    public Car (String splitedName) {
         carName = new Name(splitedName);
         carPosition = new Position(0);
     }
     public String getCarName() {return carName.getName();}
     public int getCarPosition() {return carPosition.getPosition();}
+
+    public void go(int randomNumber) {
+        if(randomNumber < 4) {
+            return;
+        }
+        carPosition.setPosition(carPosition.getPosition()+1);
+    }
 }
