02b28a700a06bbb41d63a0b8c7f3f17bd01270bd
ekore <yonghwa95@naver.com>
2023-08-08 21:30:16 +0900

    refactor controller 책임 명확 , application 클래스는 오로지 GameService 클래스만 의존

--- src/main/java/RacingGame/RacingGameApplication.java
+++ src/main/java/RacingGame/RacingGameApplication.java
@@ -7,13 +7,9 @@ import RacingGame.view.InputView;
 
 public class RacingGameApplication {
     public static void main(String args[]) {
-        InputView input = new InputView();
-        Parser parser = new Parser();
         GameService game = new GameService();
 
-        String[] carNames = parser.splitInputNames(input.requestCarName());
-        Cars joinedCars = game.joinCar(carNames);
-
+        Cars joinedCars = game.joinCar(game.splitInputString());
         game.inputRounds();
         game.playRound(joinedCars);
     }
