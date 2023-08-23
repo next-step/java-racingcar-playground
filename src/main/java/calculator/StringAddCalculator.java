82c54c08388146626e0171b2d1187f7560fee4de
pmh5574 <g_a_m_e_@naver.com>
2023-08-23 10:43:37 +0900

    splitAndSum 완료

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,25 +1,55 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
-    public static int splitAndSum(String text) {
+    public int splitAndSum(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
+
+        Integer tokens = getNewPattern(text);
+        if (tokens != null) {
+            return tokens;
+        }
+
         String[] numbers = text.split("[,:]");
-
-        int result = getSum(numbers);
-
-
-        return result;
+        return getSum(numbers);
     }
 
-    private static int getSum(String[] numbers) {
+    private Integer getNewPattern(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)")
+                .matcher(text);
+
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+
+            return getSum(m.group(2)
+                    .split(customDelimiter));
+        }
+
+        return null;
+    }
+
+    private int getSum(String[] numbers) {
+        if (minusCheck(numbers)) {
+            throw new RuntimeException();
+        }
+
         return Arrays.stream(numbers)
                 .mapToInt(Integer::parseInt)
                 .sum();
+    }
+
+    private boolean minusCheck(String[] numbers) {
+
+        return Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
+                .anyMatch(number -> number < 0);
+
     }
 
 
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,42 +8,48 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringAddCalculatorTest {
 
+    private StringAddCalculator stringAddCalculator;
+    @BeforeEach
+    void setUp() {
+        this.stringAddCalculator = new StringAddCalculator();
+    }
+
     @Test
     public void splitAndSum_null_또는_빈문자() {
-        int result = StringAddCalculator.splitAndSum(null);
+        int result = stringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
 
-        result = StringAddCalculator.splitAndSum("");
+        result = stringAddCalculator.splitAndSum("");
         assertThat(result).isEqualTo(0);
     }
 
     @Test
     public void splitAndSum_숫자하나() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1");
+        int result = stringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     public void splitAndSum_쉼표구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2");
+        int result = stringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2:3");
+        int result = stringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_custom_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        int result = stringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_negative() throws Exception {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+        assertThatThrownBy(() -> stringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 }
