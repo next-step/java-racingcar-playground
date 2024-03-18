1ee8c863f7062d0b080fca000a0652db9c994a59
miro <zeze@jbnu.ac.kr>
2024-03-18 23:39:10 +0900

    refactor(OutputView) : 빈줄 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -17,11 +17,27 @@ public class OutputView {
         return view;
     }
 
-    public void printRoundResult(List<Car> cars) {
+    public void printResultMessage() {
         System.out.println("실행 결과");
+    }
+
+    public void printRoundResult(List<Car> cars) {
+        System.out.println();
         for (Car car : cars) {
             System.out.println(car);
         }
         System.out.println();
+    }
+
+    public void printGameResult(List<Car> winners) {
+        StringBuilder sb = new StringBuilder();
+        for (Car winner : winners) {
+            sb.append(winner.getName()).append(", ");
+        }
+        if (!sb.isEmpty()) {
+            sb.delete(sb.length()-2, sb.length());
+            sb.append("가 최종 우승했습니다.");
+        }
+        System.out.println(sb.toString());
     }
 }
